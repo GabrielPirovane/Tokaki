b660f6e156d9a54f81d58cfd914a7584b397015d
gabrielpirovanemiguel <gabriel.balique@gmail.com>
2025-08-07 00:26:30 -0300

    rotas públicas adicionadas. todos os arquivos html e css das rotas públicas criados e organizados

--- routes/public.py
+++ routes/public.py
@@ -9,16 +9,51 @@ templates = Jinja2Templates(directory="templates")
 
 @router.get("/")
 async def get_root():
-    response = templates.TemplateResponse("home.html", {"request": {}})
+    response = templates.TemplateResponse("/public/home.html", {"request": {}})
     return response
 
 @router.get("/cadastro")
 async def get_cadastro():
     uf = uf_repo.UfRepo("dados.db").get_all()
-    response = templates.TemplateResponse("cadastro.html", {"request": {}, "uf":uf})
+    response = templates.TemplateResponse("/public/cadastro.html", {"request": {}, "uf":uf})
     return response
 
 @router.get("/login")
 async def get_login():
-    response = templates.TemplateResponse("login.html", {"request": {}})
+    response = templates.TemplateResponse("/public/login.html", {"request": {}})
+    return response
+
+@router.get("/verificacao")
+async def get_verificacao():
+    response = templates.TemplateResponse("/public/verificacao.html", {"request": {}})
+    return response
+
+@router.get("/sobre")
+async def get_sobre():
+    response = templates.TemplateResponse("/public/sobre.html", {"request": {}})
+    return response
+
+@router.get("/contatos")
+async def get_contatos():
+    response = templates.TemplateResponse("/public/contatos.html", {"request": {}})
+    return response
+
+@router.get("/catalogo")
+async def get_catalogo():
+    response = templates.TemplateResponse("/public/catalogo.html", {"request": {}})
+    return response
+
+@router.get("/detalhes")
+async def get_detalhes():
+    response = templates.TemplateResponse("/public/detalhes_musico.html", {"request": {}})
+    return response
+
+@router.get("/galeria")
+async def get_galeria():
+    response = templates.TemplateResponse("/public/galeria.html", {"request": {}})
+    return response
+
+@router.get("/foto")
+async def get_foto():
+    response = templates.TemplateResponse("/public/ampliar_foto.html", {"request": {}})
     return response
